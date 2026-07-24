065642334809b8317bb3d842632b344a109b72c6
dhananjaichoubey-cpu <dhananjai.choubey@gmail.com>
2026-07-24 23:09:31 +0530

    App.py

--- App.py
+++ App.py
@@ -6,6 +6,17 @@ from streamlit_lightweight_charts import renderLightweightCharts
 st.set_page_config(layout="wide")
 
 st.title("My Trading Chart")
+data = yf.download("^NSEI", period="5d", interval="5m")
+candles = []
+
+for i in data.index:
+    candles.append({
+        "time": i.strftime("%Y-%m-%d %H:%M:%S"),
+        "open": float(data.loc[i]["Open"]),
+        "high": float(data.loc[i]["High"]),
+        "low": float(data.loc[i]["Low"]),
+        "close": float(data.loc[i]["Close"]),
+    })
 
 chart = [{
     "chart": {
@@ -13,11 +24,7 @@ chart = [{
     },
     "series": [{
         "type": "Candlestick",
-        "data": [
-            {"time": "2026-07-21", "open": 25000, "high": 25100, "low": 24950, "close": 25080},
-            {"time": "2026-07-22", "open": 25080, "high": 25200, "low": 25050, "close": 25150},
-            {"time": "2026-07-23", "open": 25150, "high": 25250, "low": 25100, "close": 25220}
-        ]
+        "data": candles          
     }]
 }]
 
